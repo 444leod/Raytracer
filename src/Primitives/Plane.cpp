d9a7fdd09ef3dd52fffc833e05cc8fd4c16fd0bf
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-04-23 19:19:26 +0200

    feat(planes): added k factor to hit

--- src/Primitives/Plane.cpp
+++ src/Primitives/Plane.cpp
@@ -8,15 +8,26 @@
 #include "Plane.hpp"
 
 rtx::Plane::Plane(Color color, Vector3d position, Vector3d normal)
-    : APrimitive(position, color), _normal(normal)
+    : APrimitive(position, color), _normal(normal.normalized())
 {
 }
 
 /**
  * Maths be like:
- *  dot product. :)
+ *  dot = the dot between the ray dir and plane's normal
+ *        if dot >= 0, then ray won't hit.
+ *
+ *  k = is the parameter that will be used to get the point.
+ *  do some in between magic found online...
 */
 bool rtx::Plane::hits(const Ray& ray) const
 {
-    return this->_normal.dot(ray.direction()) < 0;
+    double k = 0;
+    Vector3d origin = this->_position - ray.origin();
+    double dot = this->_normal.dot(ray.direction().normalized());
+
+    if (dot <= 0)
+        return false;
+    k = origin.dot(this->_normal) / dot;
+    return true;
 }
