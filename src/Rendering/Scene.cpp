d6a0ec82037ad364b34bc4c879c5d16d6aaf4f99
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-05-03 13:30:59 +0200

    refactor(scene): to use HitResult & Lights

--- src/Rendering/Scene.cpp
+++ src/Rendering/Scene.cpp
@@ -10,32 +10,16 @@
 rtx::Scene::Scene(const Camera& camera)
     : _camera(camera)
 {
-    _primitives.push_back(std::make_shared<Sphere>(Color(100, 255, 100), Vector3d(7, 10, 0), 3));
-    _primitives.push_back(std::make_shared<Sphere>(Color(255, 100, 100), Vector3d(10, 0, 0), 4));
-    _primitives.push_back(std::make_shared<Sphere>(Color(100, 100, 255), Vector3d(12, -5, 0), 5));
-    _primitives.push_back(std::make_shared<Plane>(Color(100, 100, 100), Vector3d(0, 0, -2), Vector3d(0, 0, -1)));
+    _primitives.push_back(std::make_shared<Sphere>(Color(100, 100, 100), Vector3d(9, 0, 2.5), 0.5));
+    _primitives.push_back(std::make_shared<Sphere>(Color(100, 255, 100), Vector3d(5, -5, -1), 1.5));
+    _primitives.push_back(std::make_shared<Sphere>(Color(100, 100, 255), Vector3d(11, 0, 0), 2));
+    _primitives.push_back(std::make_shared<Sphere>(Color(255, 100, 100), Vector3d(7, 5, 1), 1.75));
+    _primitives.push_back(std::make_shared<Sphere>(Color(100, 100, 100), Vector3d(0, 0, -100002), 100000));
+
+    _lights.push_back(Light(Vector3d(8, 0, 8), 50.0));
 }
 
-const std::vector<rtx::Color> rtx::Scene::renderRaw(std::uint32_t width, std::uint32_t height) const
-{
-    std::vector<rtx::Color> image;
-
-    for (std::uint32_t h = 0; h < height; h++) {
-        double v = (double)h / (double)height;
-        for (std::uint32_t w = 0; w < width; w++) {
-            double u = (double)w / (double)width;
-            const Ray& ray = this->_camera.ray(u, v);
-            for (auto s : this->_primitives) {
-                if (!s->hits(ray)) continue;
-                image.push_back(s->color());
-                break;
-            }
-        }
-    }
-    return image;
-}
-
-sf::Image rtx::Scene::renderImage(std::uint32_t width, std::uint32_t height) const
+sf::Image rtx::Scene::render(std::uint32_t width, std::uint32_t height) const
 {
     auto image = sf::Image();
     image.create(width, height);
@@ -45,13 +29,21 @@ sf::Image rtx::Scene::renderImage(std::uint32_t width, std::uint32_t height) con
         for (std::uint32_t w = 0; w < width; w++) {
             double u = (double)w / (double)width;
             const Ray& ray = this->_camera.ray(u, v);
-            for (auto s : this->_primitives) {
-                if (!s->hits(ray)) continue;
-                sf::Color c = {s->color().r, s->color().g, s->color().b};
-                image.setPixel(w, h, c);
-                break;
-            }
+
+            auto hit = this->simulateRay(ray);
+            if (hit.has_value())
+                image.setPixel(w, h, hit.value().color().asSf());
         }
     }
     return image;
+}
+
+std::optional<rtx::HitResult> rtx::Scene::simulateRay(const rtx::Ray& ray) const
+{
+    for (const auto& prim : this->_primitives) {
+        auto hit = prim->hits(ray);
+        if (hit.has_value())
+            return hit.value();
+    }
+    return std::nullopt;
 }
--- src/Rendering/Scene.hpp
+++ src/Rendering/Scene.hpp
@@ -14,6 +14,7 @@
 #include "Camera.hpp"
 #include "Sphere.hpp"
 #include "Plane.hpp"
+#include "Light.hpp"
 
 namespace rtx {
     class Scene {
@@ -24,12 +25,13 @@ namespace rtx {
             const Camera& camera() const { return _camera; }
             const Camera& camera() { return _camera; }
 
-            const std::vector<Color> renderRaw(std::uint32_t width, std::uint32_t height) const;
-            sf::Image renderImage(std::uint32_t width, std::uint32_t height) const;
+            sf::Image render(std::uint32_t width, std::uint32_t height) const;
+            std::optional<rtx::HitResult> simulateRay(const rtx::Ray& ray) const;
 
         protected:
         private:
             std::vector<std::shared_ptr<IPrimitive>> _primitives;
+            std::vector<Light> _lights;
             const Camera& _camera;
     };
 }
