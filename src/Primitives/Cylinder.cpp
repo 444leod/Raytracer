2d953a6fa19228999bf71b938ddd494b4ea3983a
Léo DUMONT <leo.dumont@epitech.eu>
2024-05-09 14:32:22 +0200

    feat(cylinder): coded primitive mathematics.

--- src/Primitives/Cylinder.cpp
+++ src/Primitives/Cylinder.cpp
@@ -26,4 +26,32 @@ rtx::Cylinder::Cylinder(Color color, Vector3d position, Vector3d axis, double ra
 */
 std::optional<rtx::HitResult> rtx::Cylinder::hits(const rtx::Ray& ray) const
 {
+    auto o = ray.origin() - this->_position;
+    auto d = ray.direction();
+
+    auto x = _axis;
+    auto p = _position;
+    auto r = _radius;
+
+    Vector3d op = o - p;
+    double a1 = d.dot(x);
+    double a2 = op.dot(x);
+
+    double a = 1 - std::pow(a1, 2);
+    double b = 2 * (d.dot(op) - a1 * a2);
+    double c = op.dot(op) - std::pow(a2, 2) - std::pow(r, 2);
+
+    auto delta = Delta(a, b, c).solve();
+    if (!delta.has_value())
+        return std::nullopt;
+    if (delta.value().first < 0.01 && delta.value().second < 0.01)
+        return std::nullopt;
+    double k = delta.value().first;
+    auto i = ray.origin() + ray.direction().normalized() * k;
+
+    auto v = i - p;
+    auto projection = x * v.dot(x);
+    auto n = v - projection;
+
+    return HitResult(p, n.normalized(), this->_color);
 }
