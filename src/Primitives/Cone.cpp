c60578f46957549951eb8513ab247949e87081d4
Léo DUMONT <leo.dumont@epitech.eu>
2024-05-09 13:25:10 +0200

    feat(cone maths): added quadratic formulas values for cone intersection with a ray.

--- src/Primitives/Cone.cpp
+++ src/Primitives/Cone.cpp
@@ -13,6 +13,17 @@ rtx::Cone::Cone(Color color, Vector3d apex, Vector3d axis, double theta)
 {
 }
 
+/**
+ * Maths be like:
+ *
+ *  => a*k² + b*k + c = 0
+ *      where   a = d.v² - cos²(θ)
+ *              b = 2 * (d.v * o.v - d.o * cos²(θ))
+ *              c = o.v² - o.o * cos²(θ)
+ *
+ *  It can be solved usic quadratic formula:
+ *      D = b² - 4 * a * c
+*/
 std::optional<rtx::HitResult> rtx::Cone::hits(const rtx::Ray& ray) const
 {
 }
