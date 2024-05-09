8314b967fd3468a4e3cd5382af097c3a2c1b49f6
Léo DUMONT <leo.dumont@epitech.eu>
2024-05-09 14:31:53 +0200

    feat(cylinder): added base primitive mathematics.

--- src/Primitives/Cylinder.cpp
+++ src/Primitives/Cylinder.cpp
@@ -13,6 +13,17 @@ rtx::Cylinder::Cylinder(Color color, Vector3d position, Vector3d axis, double ra
 {
 }
 
+/**
+ * Maths be like:
+ *
+ *  => a*k² + b*k + c = 0
+ *      where   a = 1 - (d dot x)²
+ *              b = 2 * (d dot (o - p) - (d dot x) * (o - p) dot x)
+ *              c = (o - p) dot (o - p) - ((o - p) dot x)² - r²
+ *
+ *  It can be solved usic quadratic formula:
+ *      D = b² - 4 * a * c
+*/
 std::optional<rtx::HitResult> rtx::Cylinder::hits(const rtx::Ray& ray) const
 {
 }
