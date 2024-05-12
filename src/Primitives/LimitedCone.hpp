9003b40fea4da85569c94bdf7239b13f7f3b05ed
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-05-12 13:48:43 +0200

    fix(configParsing): various fixes

--- src/Primitives/LimitedCone.hpp
+++ src/Primitives/LimitedCone.hpp
@@ -18,7 +18,10 @@ namespace rtx {
 
             virtual std::optional<HitResult> hits(const Ray& ray) const;
             bool _is_not_shadow(double k, const rtx::Ray& ray) const;
-            void setApex(const Vector3d& apex) { this->_apex = apex; }
+            void setApex(const Vector3d& apex) {
+                this->_apex = apex;
+                this->_position = apex;
+            }
             void setAxis(const Vector3d& axis) { this->_axis = axis.normalized(); }
             void setTheta(const double& theta) { this->_theta = theta; }
             void setHeight(const double& height) { this->_height = height; }
